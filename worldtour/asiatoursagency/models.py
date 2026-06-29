aba81c29945315d65e4be397f689a929c686a7ee
Theophilus-git <theophilusofori819@gmail.com>
2026-06-29 08:33:08 +0000

    set up my database model, I also migrated the database

--- worldtour/asiatoursagency/models.py
+++ worldtour/asiatoursagency/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class tour(models.Model):
+    # We need a origin country
+    # We need a destination country
+    # we need number of nights
+    # we need price
+
+    orign_country = models.CharField(max_length=64)
+    destination_country = models.CharField(max_length=64)
+    number_of_nights = models.IntegerField()
+    price = models.IntegerField()
+
+    # Object Relation Mapping : Takes care of create, delete, read, and update
