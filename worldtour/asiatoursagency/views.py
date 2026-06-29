cdf098bc8e494d4390b9a8c1bafcc9c27cd3d322
Theophilus-git <theophilusofori819@gmail.com>
2026-06-29 10:20:33 +0000

    Connected the data created to an html page

--- worldtour/asiatoursagency/views.py
+++ worldtour/asiatoursagency/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from .models import tour
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to Asia Tours Agency")
+    tours = tour.objects.all()
+    context = {'tours':tours}
+    return render(request,'tours/index.html', context)
