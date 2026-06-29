8666659bc04360ccf417948f450d33bf47117349
Theophilus-git <theophilusofori819@gmail.com>
2026-06-29 09:54:40 +0000

    added data to my database model using the python shell, everything worked perfectly

--- worldtour/asiatoursagency/models.py
+++ worldtour/asiatoursagency/models.py
@@ -13,3 +13,7 @@ class tour(models.Model):
     price = models.IntegerField()
 
     # Object Relation Mapping : Takes care of create, delete, read, and update
+    
+    # This is a string representation of the tours
+    def __str__(self):
+        return (f"ID:{self.id}: From {self.orign_country} to {self.destination_country}, {self.number_of_nights} nigths cost ${self.price}")
